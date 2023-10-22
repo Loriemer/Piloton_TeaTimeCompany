de7b3798937f2c145abefaaeb8cb0b7454a3b7da
asus <asus@LAPTOP-5RR1SHUE>
2023-10-23 01:51:09 +0800

    Add project files.

---
+++ Piloton_TeaTimeCompany/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        const int shippingfee = 15;
+
+        Console.WriteLine("Enter the no. of pound: ");
+        int noOfPound = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the price per pound: ");
+        double pricePerPoud = double.Parse(Console.ReadLine());
+
+        Console.Write("Add shippping fee (Y/N): ");
+        char shippinginformation = Console.ReadLine()[0];
+
+
+        double totalamount = noOfPound * pricePerPoud;
+
+        if (shippinginformation == 'Y')
+        {
+            totalamount = totalamount + shippingfee;
+        }
+        Console.WriteLine("Total Amount: {0:F2}", totalamount);
+
+        Console.ReadKey();
+    }
+}
